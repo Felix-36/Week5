7eda401718b065e3efb1ac30d54d3eab2a8b5329
Felix Quinones <60441061+Felix-36@users.noreply.github.com>
2020-02-24 12:41:49 -0500

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,22 @@
+package week5;
+/**
+ * 
+ * Author Felix
+ * 2.24.2020
+ */
+
+import java.util.Scanner;
+
+public class RandomNumber {
+	public static void main(String[] arg) { 
+		Scanner kb= new Scanner(System.in);
+		System.out.println("Welcome, guess a number from 1 through 10");
+		
+		int i= kb.nextInt();
+		if(i==7);
+			System.out.println("Victory,Vicory,Victory");
+			else
+	System.out.println("Game Over, Game Over, Game Over");
+	}
+
+}
